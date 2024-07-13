f4f6473836e2dc47d34e267f0039e89a50d09bab
Justin Castilla <justin.castilla@elastic.co>
2024-07-12 17:33:23 -0700

    removes vercel files, updates to bulk indexing

--- api/main.py
+++ api/main.py
@@ -1,21 +1,31 @@
 import requests
 import os
-
 import logging
-import elasticsearch
+from elasticsearch import Elasticsearch, helpers
 import datetime
 
 logging.basicConfig(filename='app.log', level="INFO", format='%(asctime)s %(levelname)s %(message)s')
 logger = logging.getLogger()
 
-# Create an instance of Elasticsearch
-es = elasticsearch.Elasticsearch(os.environ.get("es_server"), api_key=os.environ.get("es_api"))
+purchase_index = "purchases-test"
 
-# fuction name: get_sales_from_bc purpose: get sales from api every 2 minutes and iterate through the purchases and index them in elastic search
+# Create an instance of Elasticsearch
+es = Elasticsearch(os.environ.get("es_server"), api_key=os.environ.get("es_api"))
+
+full_item_type = {
+    "t": "track",
+    "a": "album",
+    "p": "merch",
+    "b": "bundle"
+}
+
+bulk_purchases = []
+
+# fuction name: get_sales_from_bc purpose: get sales from api  and iterate through the purchases to index purchases in elastic search
 def get_sales_from_bc():
     try:
         # get the sales from the bandcamp API
-        response = requests.get("https://bandcamp.com/api/salesfeed/1/get?start=0&count=50")
+        response = requests.get("https://bandcamp.com/api/salesfeed/1/get")
         response = response.json()
         response = response["events"]
     
@@ -27,67 +37,140 @@ def get_sales_from_bc():
         logging.error(f"Error fetching sales from Bandcamp API: {e}")
         return []
 
-def add_purchases(purchases):
-    for purchase in purchases:
-        # handle case of multiple items per purchase
-        if type(purchase) == list:
-            for item in purchase:
-                add_purchase(item)
-        add_purchase(purchase)
 
-    print(f"Purchases added: {len(purchases)}") 
+def add_purchases_to_bulk(purchases):
+    for per_event_purchase in purchases:
+        for individual_purchase in per_event_purchase['items']:
+            ready_purchase = clean_purchase(individual_purchase)
+            bulk_purchases.append(ready_purchase)
 
 def clean_purchase(purchase):
-    purchase = purchase["items"][0]
-    purchase_utc = purchase["utc_date"]
-    utc_datetime = datetime.datetime.fromtimestamp(purchase_utc)
-    timestamp = utc_datetime.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
-    stripped_purchase = {
+    utc_datetime = datetime.datetime.fromtimestamp(purchase["utc_date"])
+    cleaned_purchase = {
         "utc_date": purchase["utc_date"],
-        "timestamp": timestamp,
         "artist_name": purchase["artist_name"],
-        "album_title": purchase["album_title"],
-        "item_type": purchase["item_type"],
         "item_description": purchase["item_description"],
+        "album_title": purchase["album_title"],
         "currency": purchase["currency"],
-        "item_price": purchase["item_price"],
         "amount_paid": purchase["amount_paid"],
-        "amount_paid_usd": round(purchase["amount_paid_usd"], 2),
-        "country": purchase["country"] or None,
-        "country_code": purchase["country_code"].upper(),
-        "url": "https:" + purchase["url"],
-        "art_url": purchase["art_url"],
-        "account_username": purchase["url"].split("//")[1].split(".bandcamp.com")[0],
+        "item_price": purchase["item_price"],
+        "amount_paid_usd": purchase["amount_paid_usd"],
+        "country": purchase["country"],
+        "timestamp":  utc_datetime.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+        "country_code":  None or purchase["country_code"].upper(),
+        "account_username":  purchase["url"].split("//")[1].split(".bandcamp.com")[0],
+        "item_price":  round(purchase["item_price"], 2),
+        "amount_paid":  round(purchase["amount_paid"], 2),
+        "amount_paid_usd":  round(purchase["amount_paid_usd"], 2),
+        "item_type":  full_item_type[purchase["item_type"]],
     }
 
-    return stripped_purchase
+    return cleaned_purchase
 
 
-def add_purchase(purchase):
-    """
-    Endpoint to add a purchase.
-
-    Args:
-        purchase (Purchase): The purchase object.
-
-    Returns:
-        dict: A dictionary with a success message and the payload of the purchase.
-    """
+def bulk_add_purchases_to_elastic(purchases):
+    try:
+        actions = []
+        for purchase in purchases:
+            action = {
+                "_index": purchase_index,
+                "_id": purchase["utc_date"],
+                "_source": purchase
+            }
+            actions.append(action)
+        
+        response = helpers.bulk(es, actions)
+        if response[1]:
+            logger.error("Error adding purchases to Elasticsearch")
+            logger.error(response[1])
+        else:
+            print(f"Purchases added to Elasticsearch: {response[0]}")
+            logger.info("Purchases added to Elasticsearch")
     
-    # prep purchase for adding to es index 
-    purchase = clean_purchase(purchase)
+    except Exception as e:
+        logger.error(f"Error adding purchases to Elasticsearch: {e}")
 
-    # Add the purchase to the purchases index
-    new_purchase = es.index(index="purchases", body=purchase)
-    logger.info(f"Purchase added: {purchase['album_title']}")
-    
-    # Return a success message
-    return {
-        "message": "Purchase added successfully",
-        "artist_update": bool(new_purchase["_shards"]["successful"]),
-    }
+
+def remove_art_url_from_purchases():
+    try:
+        # Retrieve all documents from the purchases index
+        query = {
+            "query": {
+                "match_all": {}
+            },
+            "size": 10000  # Increase the size to retrieve more documents at once
+        }
+        response = es.search(index=purchase_index, body=query, scroll="5m")
+        scroll_id = response["_scroll_id"]
+        hits = response["hits"]["hits"]
+
+        # Prepare bulk update requests
+        bulk_requests = []
+
+        # Remove the art_url field from each document
+        for hit in hits:
+            doc_id = hit["_id"]
+            doc = hit["_source"]
+            if "item_type" in doc:
+                del doc["item_type"]
+                # Add update request to bulk requests
+                bulk_requests.append({
+                    "_op_type": "update",
+                    "_index": purchase_index,
+                    "_id": doc_id,
+                    "doc": doc
+                })
+
+        # Execute bulk update requests
+        response = helpers.bulk(es, bulk_requests)
+
+        if response[1]:
+            logger.error("Error removing art_url field from purchases")
+            logger.error(response[1])
+        else:
+            print(f"Art_url field removed from purchases: {response[0]}")
+            logger.info("Art_url field removed from purchases")
+
+        # Scroll through the remaining documents
+        while hits:
+            response = es.scroll(scroll_id=scroll_id, scroll="5m")
+            scroll_id = response["_scroll_id"]
+            hits = response["hits"]["hits"]
+
+            # Prepare bulk update requests
+            bulk_requests = []
+
+            # Remove the art_url field from each document
+            for hit in hits:
+                doc_id = hit["_id"]
+                doc = hit["_source"]
+                if "item_type" in doc:
+                    del doc["item_type"]
+                    # Add update request to bulk requests
+                    bulk_requests.append({
+                        "_op_type": "update",
+                        "_index": purchase_index,
+                        "_id": doc_id,
+                        "doc": doc
+                    })
+
+            # Execute bulk update requests
+            response = helpers.bulk(es, bulk_requests)
+            print(response)
+            if response[1]:
+                logger.error("Error removing art_url field from purchases")
+                logger.error(response[1])
+            else:
+                print(f"Art_url field removed from purchases: {response[0]}")
+                logger.info("Art_url field removed from purchases")
+
+    except Exception as e:
+        logger.error(f"Error removing art_url field from purchases: {e}")
+
 
 purchases = get_sales_from_bc()
-add_purchases(purchases)
+add_purchases_to_bulk(purchases)
+bulk_add_purchases_to_elastic(bulk_purchases)
+
 logger.info("Job's Done!")
